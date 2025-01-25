5a0b28becd73cb5b2cbbfb05a905a5027a8120c2
Freddy Bijanja <freddybijanja31@gmail.com>
2025-01-25 19:00:27 +0200

    add responsive

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,18 +5,18 @@ const Head = () => {
     <>
     
     <div className="bg-[url('/public/images/code-comp.png')]  h-72  bg-no-repeat bg-center  bg-cover flex items-center justify-center">
-      <div className="flex items-center relative">
+      <div className="flex  items-center relative">
 
-        <div className="w-40 h-40 absolute z-10">
+        <div className="  w-40  h-40   absolute z-10">
             <img src="/public/images/computer.png" alt="" className="rounded-full w-full h-full   " />
         </div>
           
         <div className="relative left-28">
         <div>
-        <div className="bg-gray-950 text-white p-4 w-80  rounded-r-full ">
+        <div className="bg-gray-950 text-white p-4  2xl:w-80    rounded-r-full ">
            <h1 className="ml-10 uppercase  text-2xl" >freddy bijanja</h1>
         </div>
-        <div className="bg-white text-black w-64 p-4 rounded-r-full  ">
+        <div className="bg-white text-black  2xl:w-64 p-4 rounded-r-full  ">
             <p className="ml-10 capitalize text-sm font-bold ">software developper</p>
       </div>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import Grade from './Grade'
 
 const Main = () => {
   return (
-    <div className='grid grid-cols-2  '>
+    <div className=' 2xl:grid 2xl:grid-cols-2  xl:grid xl:grid-cols-2 lg:grid lg:grid-cols-2  md:grid md:grid-cols-1 sm:grid sm:grid-cols-1   '>
         <Presentation/>
         <Grade/>
     </div>
